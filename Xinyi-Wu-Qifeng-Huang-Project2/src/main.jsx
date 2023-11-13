a81e950c4c6d059d271b056c37489183bcdd1a6f
Krystal Wu <kk@Krystals-MacBook-Air.local>
2023-11-13 15:24:23 -0800

    pages

--- Xinyi-Wu-Qifeng-Huang-Project2/src/main.jsx
+++ Xinyi-Wu-Qifeng-Huang-Project2/src/main.jsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/rules',
+    element:<Rules />
+  },
+  {
+    path: '/game',
+    element:<Games />
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CountProvider>
+      <RouterProvider router={router} />
+      <App />{/* same as calling App() */}
+    </CountProvider>
     <App />
   </React.StrictMode>,
 )
