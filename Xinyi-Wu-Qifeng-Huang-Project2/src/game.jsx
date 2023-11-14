f3d0d907d1a461d32313aba916197c312f9d26cb
Qifeng Huang <qifenghuang@Qifengs-MacBook-Air.local>
2023-11-13 16:27:54 -0800

    Added Header

--- Xinyi-Wu-Qifeng-Huang-Project2/src/game.jsx
+++ Xinyi-Wu-Qifeng-Huang-Project2/src/game.jsx
@@ -1,7 +1,12 @@
+import Header from "./Header";
+
 export default function game(){
     return (
         <div>
+            <Header />
+            <div>
             {'This is game page.'}
+            </div>
         </div>
     )
 
--- Xinyi-Wu-Qifeng-Huang-Project2/src/rules.jsx
+++ Xinyi-Wu-Qifeng-Huang-Project2/src/rules.jsx
@@ -1,7 +1,13 @@
+import Header from "./Header";
+
 export default function rules(){
     return (
         <div>
+            <Header />
+            <div>
             {'This is rules page.'}
+            </div>
+
         </div>
     )
 
